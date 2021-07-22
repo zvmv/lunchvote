3423bffa8d179a3d1ec6d9287058087d2561cd49
zvmv <zvonilov_kst@mts-nn.ru>
2021-07-22 10:08:50 +0300

    new first commit

---
+++ src/test/java/ru/pet/lunchvote/LunchvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.lunchvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchvoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
