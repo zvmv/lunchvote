a46f9e4e883fb80e7bf9a708a93b2060d1f91cb9
zvmv <zvonilov_kst@mts-nn.ru>
2021-07-28 20:13:41 +0300

    Chore cleanup and add some comments to app.config

--- src/main/java/ru/pet/lunchvote/repository/GenericRepostory.java
+++
@@ -1,6 +0,0 @@
-package ru.pet.lunchvote.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenericRepostory<T> extends JpaRepository<T, Integer> {
-}
--- src/main/resources/db/import.sql
+++ src/main/resources/db/import.sql
@@ -14,6 +14,6 @@ VALUES ('Мак дональдс', 'пл.Ленина'),
 
 INSERT INTO menu (restaurant_id, menudate, dishes, price)
 VALUES (1, CURRENT_DATE, 'Бигтейсти, картошка фри, кола', 300),
-       (2, CURRENT_DATE, 'Салат, солянка, чай/кофе', 400 ),
+       (2, CURRENT_DATE, 'Салат, солянка, чай/кофе', 400),
        (3, CURRENT_DATE, 'Пицца "Пепперони"', 450),
-       (4, CURRENT_DATE, 'Филадельфия роллы', 400)
+       (4, CURRENT_DATE, 'Филадельфия роллы', 400);
