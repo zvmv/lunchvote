fb22f2a6bae71c858363fef45350eb483c856ced
zvmv <zvonilov_kst@mts-nn.ru>
2021-07-29 11:56:22 +0300

    Refactor Extend UserController from Generic

--- src/main/java/ru/pet/lunchvote/controller/UserRestController.java
+++ src/main/java/ru/pet/lunchvote/controller/UserRestController.java
@@ -2,6 +2,7 @@ package ru.pet.lunchvote.controller;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import ru.pet.lunchvote.model.User;
 import ru.pet.lunchvote.repository.UserRepository;
@@ -10,34 +11,10 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 @RestController
-public class UserRestController {
-    private UserRepository repository;
-
-    public UserRestController(UserRepository repository, EntityManager em) {
-        this.repository = repository;
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> getAll(){
-      return ResponseEntity.ok(repository.findAll());
-    }
-
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> getById(@PathVariable("id") int id){
-        try { return ResponseEntity.ok(repository.getById(id)); }
-        catch (EmptyResultDataAccessException e) { return ResponseEntity.notFound().build(); }
-    }
-
-    @PostMapping("/users")
-    public ResponseEntity<User> put(@RequestBody User user){
-        repository.save(user);
-        return ResponseEntity.ok(user);
-    }
-
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") int id){
-        try { repository.deleteById(id); }
-        catch (EmptyResultDataAccessException e){ return ResponseEntity.notFound().build(); }
-        return ResponseEntity.ok().build();
+@RequestMapping("/users")
+@Transactional
+public class UserRestController extends GenericRestController<User> {
+    public UserRestController(UserRepository repository) {
+        super(repository);
     }
 }
