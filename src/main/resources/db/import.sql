9d450ad1405dbe556354ddd54dbd05391fc4ba2e
zvmv <zvonilov_kst@mts-nn.ru>
2021-10-05 20:01:32 +0300

    Feat make more users and easy pass for tests

--- src/main/resources/db/import.sql
+++ src/main/resources/db/import.sql
@@ -1,6 +1,9 @@
 INSERT INTO users (name, email, enabled, password, admin)
-VALUES ('User', 'user@yandex.ru', true, 'password', false),
-       ('Admin', 'admin@gmail.com', true, 'admin', true);
+VALUES ('Mike', 'mike@yandex.ru', true, 'pass', false),
+       ('Kelly', 'kelly@yandex.ru', true, 'pass', false),
+       ('Bob', 'bob@yandex.ru', true, 'pass', false),
+       ('Jack', 'jack@yandex.ru', true, 'pass', false),
+       ('Admin', 'admin@yandex.ru', true, 'pass', true);
 
 INSERT INTO restaurant (name, address)
 VALUES ('Мак дональдс', 'пл.Ленина'),
@@ -12,4 +15,5 @@ INSERT INTO menu (restaurant, menudate, dishes, price)
 VALUES ('Mc.Donalds', CURRENT_DATE, 'Бигтейсти, картошка фри, кола', 300),
        ('Волга', CURRENT_DATE, 'Салат, солянка, чай/кофе', 400),
        ('Мир вкуса', CURRENT_DATE, 'Пицца "Пепперони"', 450),
-       ('Автосуши', CURRENT_DATE, 'Филадельфия роллы', 400);
+       ('Автосуши', CURRENT_DATE, 'Филадельфия роллы', 400),
+       ('Столовка', CURRENT_DATE, 'Первое, второе, компот и ватрушка', 150);
