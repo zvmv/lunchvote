e05285bd2e7de5c6613f81365a788e2c3b9a6c12
zvmv <zvonilov_kst@mts-nn.ru>
2021-10-05 21:15:35 +0300

    Feat Make hashcode & equals in entities

--- src/main/java/ru/pet/lunchvote/model/AbstractBaseEntity.java
+++ src/main/java/ru/pet/lunchvote/model/AbstractBaseEntity.java
@@ -2,6 +2,7 @@ package ru.pet.lunchvote.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -13,4 +14,21 @@ abstract public class AbstractBaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
 }
