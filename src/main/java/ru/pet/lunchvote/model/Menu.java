2e0dff8a81c4824cdb208ffb2260629fd656490e
zvmv <zvonilov_kst@mts-nn.ru>
2021-10-06 21:05:45 +0300

    Fix Reject null fields

--- src/main/java/ru/pet/lunchvote/model/Menu.java
+++ src/main/java/ru/pet/lunchvote/model/Menu.java
@@ -15,16 +15,20 @@ import java.time.LocalDate;
 @Setter
 public class Menu extends AbstractBaseEntity {
     @NotNull
+    @Column(nullable = false)
     private LocalDate menudate;
 
     @NotNull
     @Max(16)
+    @Column(nullable = false)
     private String restaurant;
 
     @NotNull
     @Max(255)
+    @Column(nullable = false)
     private String dishes;
 
     @NotNull
+    @Column(nullable = false)
     private Integer price;
 }
