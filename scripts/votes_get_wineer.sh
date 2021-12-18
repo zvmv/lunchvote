f7e894303a72cc8384712c429979c4ad2d3d40ce
zvmv <zvonilov_kst@mts-nn.ru>
2021-12-18 21:40:48 +0300

    Feat Add new scripts for votes

--- scripts/votes_get_wineer.sh
+++ scripts/votes_get_wineer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-  curl -s -D /dev/stderr -X GET "http://127.0.0.1:8080/winner"  |json_pp
+  curl -s -D /dev/stderr -X GET "http://127.0.0.1:8080/votes/winner"  |json_pp
--- scripts/votes_get_wineer_byDate.sh
+++ scripts/votes_get_wineer_byDate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ x$1 != x ]; then
-  curl -s -D /dev/stderr -X GET "http://127.0.0.1:8080/winner/$1" |json_pp
+  curl -s -D /dev/stderr -X GET "http://127.0.0.1:8080/votes/winner/$1" |json_pp
 else
 	echo "Use $0 <yyyy-mm-dd>" 
 fi
